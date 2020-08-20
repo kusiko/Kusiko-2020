c4edef4abae36701b8a19fbfe1d1e1c7a791de8c
Kusiko <tikhonova.t@pm.me>
2020-08-20 08:10:01 -0400

    'pycharm_push_to_git'

---
+++ pycharm_kuski.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pandas as pd
+df = pd.read_excel(r'C:\for copy\Downloads\Live, Core, Lite Buildings - US Office - Usage Metrics SSAS.xlsx', sheet_name='cancellations')
+import matplotlib.pyplot as plt
+to_graph = df['Property Type Name'].value_counts()
+to_graph.plot(kind='bar', color='blue')
+plt.title('Property Type by FacID Count', fontsize=11)
+plt.xlabel('Building Type')
+plt.ylabel('# of FacIDs')
